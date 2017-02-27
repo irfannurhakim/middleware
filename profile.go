6e1c18a869f75e2b531bd339e4dc966515e4558c
irfan nurhakim <myfunareaa@gmail.com>
2017-02-27 17:46:17 +0700

    Add profile middleware

---
+++ profile.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"encoding/json"
+
+	"github.com/irfannurhakim/goreq"
+	"github.com/irfannurhakim/models"
+	"github.com/labstack/echo"
+)
+
+// ResponseMessage is
+type ResponseMessage struct {
+	Data models.UserProfile `json:"data"`
+}
+
+// Profile is
+func Profile(identityHost string) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) (err error) {
+			// get request object
+			req := c.Request()
+
+			// get Tenant ID
+			tenantID := c.Param("tenant_id")
+
+			// get auth header token
+			authHeader := req.Header.Get("Authorization")
+
+			request := goreq.Request{
+				Method: "GET",
+				Uri:    identityHost + "/" + tenantID + "/api/v1/me",
+			}
+
+			request.AddHeader("Authorization", authHeader)
+
+			res, err := request.Do()
+			if err != nil {
+				return err
+			}
+
+			responseMessage := new(ResponseMessage)
+			bodyStr, _ := res.Body.ToString()
+			json.Unmarshal([]byte(bodyStr), &responseMessage)
+
+			c.Set("user_profile", responseMessage.Data)
+
+			return next(c)
+		}
+	}
+}
